7552d5ee89ef5fa25326f7c211ca66297c3ad86e
Elias Norberg <xyzzy@kudzu.se>
2014-02-18 00:26:49 +0100

    Fix rendering

--- map.c
+++ map.c
@@ -1,6 +1,7 @@
 #include "animation.h"
 #include "map.h"
 #include "player.h"
+#include "sdl.h"
 
 static int 
 map_to_screen_x(adv_map *m, int x)
@@ -27,21 +28,64 @@ render_map(adv_map *m, player *p)
 {
 	int x;
 	int y;
+	int start_x, start_y;
+	int end_x, end_y;
+	int screen_adjust_x = 0, screen_adjust_y = 0;
 
+	int screen_width, screen_height;
 	x = 0;
 	y = 0;
 
-	for(y = 0; y < m->height; y ++) {
-		for(x = 0; x < m->width; x ++) {
+	screen_width = rs.screen->w / FRAME_WIDTH;
+	screen_height = rs.screen->h / FRAME_WIDTH;
+
+	if (m->width < screen_width) {
+		start_x = 0; end_x = m->width;
+		screen_adjust_x = (screen_width - (end_x - start_x))/2;
+	} else {
+		start_x = p->tile_x - screen_width / 2;
+		if (start_x < 0) start_x = 0;
+
+		end_x = p->tile_x + screen_width / 2;
+		if (end_x > m->width) end_x = m->width;
+
+		if ((end_x - start_x) < screen_width) {
+			screen_adjust_x = (end_x - start_x) / 2;
+		}
+	}
+
+	if (m->height < screen_height) {
+		start_y = 0; end_y = m->height;
+		screen_adjust_y = (screen_height - (end_y - start_y))/2;
+	} else {
+		start_y = p->tile_y - screen_height / 2;
+		if (start_y < 0) start_y = 0;
+
+		end_y = p->tile_y + screen_height / 2;
+		if (end_y > m->height) end_y = m->height;
+
+		while (((end_y - start_y) < screen_height) &&
+		    (start_y > 0 || end_y < m->height)) {
+			if (start_y > 0) start_y --;
+			if (end_y < m->height) end_y ++;
+		}
+		if ((end_y - start_y) < screen_height) {
+			screen_adjust_y = (end_y - start_y) / 2;
+		}
+	}
+
+	for(y = start_y; y < end_y; y ++) {
+		for(x = start_x; x < end_x; x ++) {
 			render_animation(m->tiles[x+y*m->width]->animation_id,
-			    map_to_screen_x(m, x), map_to_screen_y(m, y),
+			    map_to_screen_x(m, x - start_x + screen_adjust_x),
+			    map_to_screen_y(m, y - start_y + screen_adjust_y),
 			    m->tiles[x+y*m->width]->animation_frame);
 		}
 	}
 
 	render_animation(p->animation_id,
-	    map_to_screen_x(m, p->tile_x) + p->xx,
-	    map_to_screen_y(m, p->tile_y) + p->yy,
+	    map_to_screen_x(m, p->tile_x - start_x + screen_adjust_x) + p->xx,
+	    map_to_screen_y(m, p->tile_y - start_y + screen_adjust_y) + p->yy,
 	    p->animation_frame);
 	return 0;
 }
