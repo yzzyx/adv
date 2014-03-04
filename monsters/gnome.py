48f9d02e2f2dd8c41fbea3c02b242920936ed30c
Anton <anton@naetet.se>
2014-03-04 10:50:55 +0100

    Added new monster: GreenGoblinKing

--- monsters/gnome.py
+++ monsters/gnome.py
@@ -54,6 +54,41 @@ class GreenGoblin(monster.Monster):
             self.target_y = min(32, self.target_y)
         self.is_dirty = True
 
+
+class GreenGoblinKing(monster.Monster):
+    x = 10
+    y = 10
+    target_x = x
+    target_y = y
+    speed = 1
+    timer = 4
+    has_directions = 1
+    spritesheet = adv.loadSpritesheet("green_goblin_king.png")
+
+    animation_stopped = [adv.createAnimation(spritesheet, 0, 2),
+                         adv.createAnimation(spritesheet, 2, 2),
+                         adv.createAnimation(spritesheet, 4, 2),
+                         adv.createAnimation(spritesheet, 6, 2)]
+    animation_moving = [adv.createAnimation(spritesheet, 0, 2),
+                        adv.createAnimation(spritesheet, 2, 2),
+                        adv.createAnimation(spritesheet, 4, 2),
+                        adv.createAnimation(spritesheet, 6, 2)]
+
+    def getDistanceTo(self, x, y):
+        return abs(self.x - x) + abs(self.y - y)
+
+    def tick(self):
+        player = adv.getPlayer()
+        if self.getDistanceTo(player.x, player.y) < 6:
+            self.target_x, self.target_y = player.x, player.y
+        else:
+            self.target_x, self.target_y = self.x + random.randint(-1, 1), self.y + random.randint(-1, 1)
+            self.target_x = max(0, self.target_x)
+            self.target_y = max(0, self.target_y)
+            self.target_x = min(24, self.target_x)
+            self.target_y = min(32, self.target_y)
+        self.is_dirty = True
+
 """
 class SwarmGoblin(monster.Monster):
     x = 10
